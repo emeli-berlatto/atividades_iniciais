499c0a3d143b1d189263bcba01717323876a50d8
Emeli Berlatto <emeli_berlatto@estudante.sesisenai.org.br>
2024-06-26 20:56:01 -0300

    exercicio 05

---
+++ lista_01/exercicio_05/index.js
@@ -0,0 +1,13 @@
+let esconder = document.getElementById('esconder')
+
+esconder.addEventListener('click', function() {
+    document.getElementById("mensagem").style.display = "none";
+      
+})
+let mostrar = document.getElementById('mostrar')
+
+mostrar.addEventListener('click', function() {
+    document.getElementById("mensagem").style.display = "block";
+   
+      
+})
