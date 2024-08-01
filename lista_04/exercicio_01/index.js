0c24fc06630a019c14b41fc76e1fd18749e9e524
Emeli Berlatto <emeli_berlatto@estudante.sesisenai.org.br>
2024-07-31 21:10:01 -0300

    exercicio 01

---
+++ lista_04/exercicio_01/index.js
@@ -0,0 +1,12 @@
+let botao = document.getElementById('botao')
+
+botao.addEventListener('click', function() {
+    let a = document.getElementById('a').value
+    let b = document.getElementById('b').value
+
+    fetch('morango.php?a=' + a + '&b=' + b).then(function(resposta) {
+        return resposta.text();
+    }).then(function(texto) {
+        document.getElementById('mensagem').innerText = texto
+    })
+})
