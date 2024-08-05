6986680bb083ca93901999e82723a1ff27488e1b
Emeli Berlatto <emeli_berlatto@estudante.sesisenai.org.br>
2024-08-05 19:26:30 -0300

    exercicio 02

--- lista_04/exercicio_02/index.js
+++ lista_04/exercicio_02/index.js
@@ -4,7 +4,7 @@ botao.addEventListener('click', function() {
     let a = document.getElementById('a').value
     let b = document.getElementById('b').value
 
-    fetch('exemplo.php?a=' + a + '&b=' + b).then(function(resposta) {
+    fetch('morango.php?a=' + a + '&b=' + b).then(function(resposta) {
         return resposta.json();
     }).then(function(json) {
         document.getElementById('mensagem_antes').innerText = json['antes']
